9aceadfb383dc4b4700e4f25a821102fbb0bc631
Mike <mjreiss4@gmail.com>
2015-07-28 17:34:25 -0400

    more validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,13 @@
 class Review < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :restuarant
+
+	validates :comment, presence: true
+
+	validates :rating, numericality: {
+	  only_integer: true,
+	  greater_than_or_equal_to: 1,
+	  less_than_or_equal_to: 5,
+	  message: "can only be a whole number between 1 and 5"
+	}
 end
